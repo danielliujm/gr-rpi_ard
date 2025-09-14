97e92945b8cd0c335f0c543f086e9a7332d4357b
Danny Liu <dliujm@umich.edu>
2025-09-13 22:43:48 -0400

    9/13 commit

--- ard_ctrl/ard_ctrl.ino
+++ ard_ctrl/ard_ctrl.ino
@@ -1,6 +1,8 @@
 #include <ArduinoJson.h>
 #include <Wire.h>
 #include <comm_.h>
+#include <TMC2209.h>
+#include <SoftwareSerial.h>
 
 
 
@@ -37,6 +39,54 @@ double currentPWMValues[8] = {0,0,0,0,0,0,0,0};
 int prev_time; 
 int SAVE_INTERVAL = 10000;
 
+const int RX_PIN_1 = 52;
+const int TX_PIN_1 = 49;
+const int RX_PIN_2 = 50;
+const int TX_PIN_2 = 51;
+const int RX_PIN_3 = 53;
+const int TX_PIN_3 = 48;
+
+TMC2209 stepper_drivers[6] = {};
+
+const int RUN_CURRENT_PERCENT = 100; // Set the run current to 100% of the maximum
+
+
+
+// helpers 
+int map_pwm_to_speed (double pwm){
+  return map (pwm, 0, 100, 0, 40000);
+}
+
+void setup_inlets() {
+  
+  HardwareSerial & port1  = Serial1;
+  SoftwareSerial port2 (RX_PIN_1, TX_PIN_1);
+  HardwareSerial & port3 = Serial2;
+  SoftwareSerial port4(RX_PIN_2, TX_PIN_2);
+  HardwareSerial & port5 = Serial3;
+  SoftwareSerial port6(RX_PIN_3, TX_PIN_3);
+
+  stepper_drivers[0].setup (port1);
+  stepper_drivers[1].setup (port2);
+  stepper_drivers[2].setup (port3);
+  stepper_drivers[3].setup (port4);
+  stepper_drivers[4].setup (port5);
+  stepper_drivers[5].setup (port6);
+
+
+  for (int i = 0; i< 6 ; i++){
+  stepper_drivers[i].setRunCurrent(RUN_CURRENT_PERCENT);
+  stepper_drivers[i].enableCoolStep();
+  stepper_drivers[i].enable();
+  stepper_drivers[i].setReplyDelay(8);
+  stepper_drivers[i].moveAtVelocity (20000);
+  delay (100);
+
+  }
+
+
+}
+
 void setup() {
   Serial.begin(9600);
   for (int i = 0; i < 8; i++) {
@@ -56,6 +106,8 @@ void setup() {
   activateSensor ( PH2_ADDR, 0x01);
   activateSensor ( ORP1_ADDR, 0x01);
   activateSensor ( ORP2_ADDR, 0x01);
+
+  setup_inlets();
 }
 
 void loop() {
@@ -80,44 +132,39 @@ void loop() {
   
   int FLOAT_SWITCH_STATE_1 = digitalRead(FLOAT_SWITCH_PIN_1);
   int FLOAT_SWITCH_STATE_2 = digitalRead(FLOAT_SWITCH_PIN_2);
-  if (FLOAT_SWITCH_STATE_1 == HIGH){
-    EFFLUENT_DUTY_1 = min (100, EFFLUENT_DUTY_1 + KI_EFFLUENT_1);
-    EFFLUENT_DUTY_2 = min (100, EFFLUENT_DUTY_2 + KI_EFFLUENT_1);
-    EFFLUENT_DUTY_3 = min (100, EFFLUENT_DUTY_3 + KI_EFFLUENT_1);
 
-    INLET_DUTY_1 = max (0, INLET_DUTY_1 - KI_INLET_1);
-  }
-  else if (FLOAT_SWITCH_STATE_1 == LOW){
-    EFFLUENT_DUTY_1 = max (0, EFFLUENT_DUTY_1 - KI_EFFLUENT_1);
-    EFFLUENT_DUTY_2 = max (0, EFFLUENT_DUTY_2 - KI_EFFLUENT_1);
-    EFFLUENT_DUTY_3 = max (0, EFFLUENT_DUTY_3 - KI_EFFLUENT_1);
 
-    INLET_DUTY_1 =  min (1, INLET_DUTY_1 + KI_INLET_1);
-  }
+  // if (FLOAT_SWITCH_STATE_1 == HIGH){
+  //   EFFLUENT_DUTY_1 = min (100, EFFLUENT_DUTY_1 + KI_EFFLUENT_1);
+  //   EFFLUENT_DUTY_2 = min (100, EFFLUENT_DUTY_2 + KI_EFFLUENT_1);
+  //   EFFLUENT_DUTY_3 = min (100, EFFLUENT_DUTY_3 + KI_EFFLUENT_1);
 
-  if (FLOAT_SWITCH_STATE_2 == HIGH){
-    EFFLUENT_DUTY_4 = min (100, EFFLUENT_DUTY_4 + KI_EFFLUENT_2);
-    EFFLUENT_DUTY_5 = min (100, EFFLUENT_DUTY_5 + KI_EFFLUENT_2);
-    EFFLUENT_DUTY_6 = min (100, EFFLUENT_DUTY_6 + KI_EFFLUENT_2);
+  //   INLET_DUTY_1 = max (0, INLET_DUTY_1 - KI_INLET_1);
+  // }
+  // else if (FLOAT_SWITCH_STATE_1 == LOW){
+  //   EFFLUENT_DUTY_1 = max (0, EFFLUENT_DUTY_1 - KI_EFFLUENT_1);
+  //   EFFLUENT_DUTY_2 = max (0, EFFLUENT_DUTY_2 - KI_EFFLUENT_1);
+  //   EFFLUENT_DUTY_3 = max (0, EFFLUENT_DUTY_3 - KI_EFFLUENT_1);
 
-    INLET_DUTY_2 = max (0, INLET_DUTY_2 - KI_INLET_2);
-  }
-  else if (FLOAT_SWITCH_STATE_2 == LOW){
-    EFFLUENT_DUTY_4 = max (0, EFFLUENT_DUTY_4 - KI_EFFLUENT_2);
-    EFFLUENT_DUTY_5 = max (0, EFFLUENT_DUTY_5 - KI_EFFLUENT_2);
-    EFFLUENT_DUTY_6 = max (0, EFFLUENT_DUTY_6 - KI_EFFLUENT_2);
+  //   INLET_DUTY_1 =  min (1, INLET_DUTY_1 + KI_INLET_1);
+  // }
 
-    INLET_DUTY_2 = min (100, INLET_DUTY_2 + KI_INLET_2);
-  }
+  // if (FLOAT_SWITCH_STATE_2 == HIGH){
+  //   EFFLUENT_DUTY_4 = min (100, EFFLUENT_DUTY_4 + KI_EFFLUENT_2);
+  //   EFFLUENT_DUTY_5 = min (100, EFFLUENT_DUTY_5 + KI_EFFLUENT_2);
+  //   EFFLUENT_DUTY_6 = min (100, EFFLUENT_DUTY_6 + KI_EFFLUENT_2);
 
-  analogWrite(motorPins[0], EFFLUENT_DUTY_1 * 255);
-  analogWrite(motorPins[1], EFFLUENT_DUTY_2 * 255);
-  analogWrite(motorPins[2], EFFLUENT_DUTY_3 * 255);
-  analogWrite(motorPins[3], EFFLUENT_DUTY_4 * 255);
-  analogWrite(motorPins[4], EFFLUENT_DUTY_5 * 255);
-  analogWrite(motorPins[5], EFFLUENT_DUTY_6 * 255);
-  analogWrite(motorPins[6], INLET_DUTY_1 * 255);
-  analogWrite(motorPins[7], INLET_DUTY_2 * 255);
+  //   INLET_DUTY_2 = max (0, INLET_DUTY_2 - KI_INLET_2);
+  // }
+  // else if (FLOAT_SWITCH_STATE_2 == LOW){
+  //   EFFLUENT_DUTY_4 = max (0, EFFLUENT_DUTY_4 - KI_EFFLUENT_2);
+  //   EFFLUENT_DUTY_5 = max (0, EFFLUENT_DUTY_5 - KI_EFFLUENT_2);
+  //   EFFLUENT_DUTY_6 = max (0, EFFLUENT_DUTY_6 - KI_EFFLUENT_2);
+
+  //   INLET_DUTY_2 = min (100, INLET_DUTY_2 + KI_INLET_2);
+  // }
+
+
 
   currentPWMValues[0] = EFFLUENT_DUTY_1 ;
   currentPWMValues[1] = EFFLUENT_DUTY_2  ;
@@ -127,6 +174,11 @@ void loop() {
   currentPWMValues[5] = EFFLUENT_DUTY_6  ;
   currentPWMValues[6] = INLET_DUTY_1  ;
   currentPWMValues[7] = INLET_DUTY_2  ;
+
+  for (int i = 0; i < 6; i++){
+    stepper_drivers[i].moveAtVelocity (map_pwm_to_speed (100.0));
+    // delay (10);
+  }
 
 
 
